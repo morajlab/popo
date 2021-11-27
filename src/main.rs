3403714a7d72c754dba95ea4f7c19c1687af63f5
Morteza Jamali <mortezajamali4241@gmail.com>
2021-11-27 09:15:27 +0000

    feat(base): create cli base app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use clap::{Arg, App};
+
 fn main() {
-  println!("Hello, world!");
+    let matches = App::new("POPO cli")
+        .version("0.0.1-alpha")
+        .author("Morteza Jamali <mortezajamali4241@gmail.com>")
+        .about("A perfect tool for managing monorepo")
+        .arg(Arg::new("config")
+            .short('c')
+            .long("config")
+            .value_name("FILE")
+            .about("Sets a custom config file")
+            .takes_value(true))
+        .arg(Arg::new("INPUT")
+            .about("Sets the input file to use")
+            .required(true)
+            .index(1))
+        .arg(Arg::new("v")
+            .short('v')
+            .multiple_occurrences(true)
+            .takes_value(true)
+            .about("Sets the level of verbosity"))
+        .subcommand(App::new("test")
+            .about("controls testing features")
+            .version("1.3")
+            .author("Someone E. <someone_else@other.com>")
+            .arg(Arg::new("debug")
+                .short('d')
+                .about("print debug information verbosely")))
+        .get_matches();
+
+    // You can check the value provided by positional arguments, or option arguments
+    if let Some(i) = matches.value_of("INPUT") {
+        println!("Value for input: {}", i);
+    }
+
+    if let Some(c) = matches.value_of("config") {
+        println!("Value for config: {}", c);
+    }
+
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match matches.occurrences_of("v") {
+        0 => println!("Verbose mode is off"),
+        1 => println!("Verbose mode is kind of on"),
+        2 => println!("Verbose mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level app
+    if let Some(ref matches) = matches.subcommand_matches("test") {
+        // "$ myapp test" was run
+        if matches.is_present("debug") {
+            // "$ myapp test -d" was run
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
+    // Continued program logic goes here...
 }
